fe51e284fc09780414590b03eea2c8ca4bda1b60
elye <elye.project@gmail.com>
2024-10-26 22:22:47 +1100

    Add formatting a little

--- design-kit/src/main/java/com/elyeproj/design_kit/DesignKit.kt
+++ design-kit/src/main/java/com/elyeproj/design_kit/DesignKit.kt
@@ -1,14 +1,23 @@
 package com.elyeproj.design_kit
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 
 class DesignKit {
 }
@@ -18,8 +27,6 @@ val socketListView = SocketListView()
 
 
 class SocketListView {
-
-
     lateinit var a: @Composable (String) -> Unit
 
     fun getItemFirst(data: String):  @Composable LazyItemScope.() -> Unit = {
@@ -39,4 +46,20 @@ fun SocketList(innerPadding: PaddingValues) {
         }
     }
 }
+
+
+@Composable
+fun ListPlugItem(content: @Composable () -> Unit) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(20.dp))
+            .padding(10.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .border(BorderStroke(2.dp, SolidColor(Color.Red)))) {
+        Text("Template from Plug")
+        content.invoke()
+    }
+}
+
 
--- team-b/src/main/java/com/elyeproj/team_b/MyItems.kt
+++ team-b/src/main/java/com/elyeproj/team_b/MyItems.kt
@@ -1,8 +1,17 @@
 package com.elyeproj.team_b
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.elyeproj.design_kit.ListPlugItem
 import com.elyeproj.design_kit.socketListView
 
 
@@ -13,9 +22,17 @@ fun registerViewB() {
 
 @Composable
 fun TeamBView(data: String) {
-    Column {
-        Text("This is what I try")
-        Text("Team B: $data")
+    ListPlugItem {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(Color(0xFF7DCEA0))
+        ) {
+            Text("Team B Here")
+            Text("From OutSide: $data Data")
+        }
     }
 }
 
